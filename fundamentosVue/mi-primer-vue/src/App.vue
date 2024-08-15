3ee65e8cd5dd99adb9e1f8973b1042daaf194f2e
stonxzz <cuentamlgpro@gmail.com>
2024-08-15 15:32:45 -0600

    feature: :sparkles: An example was made using a for to loop through an object

--- fundamentosVue/mi-primer-vue/src/App.vue
+++ fundamentosVue/mi-primer-vue/src/App.vue
@@ -3,31 +3,23 @@ const name = 'Vue dinamico'
 // const styleColor = "color:blue"
 // const arrayColores = ['blue', 'red', 'peru']
 // const activo=true;
-const arrayFrutas = [
+const objetoFruta = 
   {
     name: "Manzana",
     price: "$1.00",
     description: "Una manzana",
-  },
-  {
-    name: "Pera",
-    price: "$2.00",
-    description: "Una pera",
-  },
-  {
-    name: "Naranja",
-    price: "$3.00",
-    description: "Una naranja",
-  },
-];
+    id: 1
+  }
+
 </script>
 
 <template>
   <h1>Hola {{name.toUpperCase()}}</h1>
+  {{ objetoFruta }}
   <ul>
-    <li v-for="(fruta) in arrayFrutas":key="fruta.name">
-      {{ fruta.name }} - {{ fruta.price }} - {{ fruta.description }} 
-    </li>
+    <li v-for="(value, propiedad, index) in objetoFruta" :key="value">
+        {{ index }} - {{ propiedad }} : {{value}} 
+     </li>
   </ul>
   <!-- <h2>{{ arrayColores }}</h2> -->
   <!-- //: estos dos puntos es la abreviacion del vbind
