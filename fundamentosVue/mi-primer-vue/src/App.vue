3ab079f258d5ded1a9d97afbb08367939aba9cb4
stonxzz <cuentamlgpro@gmail.com>
2024-08-15 16:26:09 -0600

    feature: :sparkles: An example was made of how to use vfor and vif together, resolving the weight conflict

--- fundamentosVue/mi-primer-vue/src/App.vue
+++ fundamentosVue/mi-primer-vue/src/App.vue
@@ -1,25 +1,53 @@
 <script setup>
+const activo = true
 const name = 'Vue dinamico'
 // const styleColor = "color:blue"
 // const arrayColores = ['blue', 'red', 'peru']
 // const activo=true;
-const objetoFruta = 
+// const objetoFruta = 
+//   {
+//     name: "Manzana",
+//     price: "$1.00",
+//     description: "Una manzana",
+//     id: 1
+//   }
+
+const arrayFrutas = [
   {
     name: "Manzana",
     price: "$1.00",
     description: "Una manzana",
-    id: 1
-  }
+    stock: 0,
+  },
+  {
+    name: "Pera",
+    price: "$2.00",
+    description: "Una pera",
+    stock: 10,
+  },
+  {
+    name: "Naranja",
+    price: "$3.00",
+    description: "Una naranja",
+    stock: 20,
+  },
+];
 
 </script>
 
 <template>
   <h1>Hola {{name.toUpperCase()}}</h1>
-  {{ objetoFruta }}
+  
   <ul>
-    <li v-for="(value, propiedad, index) in objetoFruta" :key="value">
+    <!-- recorrer objetos -->
+    <!-- <li v-for="(value, propiedad, index) in objetoFruta" :key="value">
         {{ index }} - {{ propiedad }} : {{value}} 
-     </li>
+     </li> -->
+     <template v-for="(item) in arrayFrutas" :key="item.name">
+    <li v-if="item.stock>0">
+      {{ item.name }} - {{ item.price }}
+    </li>
+     </template>
   </ul>
   <!-- <h2>{{ arrayColores }}</h2> -->
   <!-- //: estos dos puntos es la abreviacion del vbind
