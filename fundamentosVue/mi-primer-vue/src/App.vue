fe63e58d5edcd9ed1149bf124a847f17d264dc19
stonxzz <cuentamlgpro@gmail.com>
2024-08-15 14:11:33 -0600

    feature: :sparkles: An example of how use velse and vshow was made

--- fundamentosVue/mi-primer-vue/src/App.vue
+++ fundamentosVue/mi-primer-vue/src/App.vue
@@ -10,9 +10,14 @@ const activo=true;
   <h2>{{ arrayColores }}</h2>
   <!-- //: estos dos puntos es la abreviacion del vbind -->
   <h2 :style="`color: ${arrayColores[2]}`">`Soy peru</h2>
-  <h2>
-    {{ activo ? 'Estoy activo ' : 'Estoy inactivo' }}
+  <h2 v-if="activo ===  true">
+    <span>Icono</span>
+    Estoy activo
   </h2>
+  <p v-else-if="activo === false">Estoy inactivo</p>
+  <p v-else>Estoy indeciso</p>
+
+  <h2 v-show="activo">Estoy activo v-show</h2>
 </template>
 
 <style>
