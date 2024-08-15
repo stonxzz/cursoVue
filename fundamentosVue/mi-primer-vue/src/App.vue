cccf2d97c8a09dba6fe6125f99d0c376b0c38b59
stonxzz <cuentamlgpro@gmail.com>
2024-08-15 13:16:14 -0600

    feature: :sparkles: An example of how vbind works was made

--- fundamentosVue/mi-primer-vue/src/App.vue
+++ fundamentosVue/mi-primer-vue/src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 const name = 'Vue dinamico'
+const styleColor = "color:blue"
 </script>
 
 <template>
   <h1>Hola {{name}}</h1>
+  <h2 :style="styleColor">Soy azul</h2>//: estos dos puntos es la abreviacion del vbind
 </template>
 
 <style>
