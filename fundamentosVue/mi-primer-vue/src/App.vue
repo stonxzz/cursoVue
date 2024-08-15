c93833f76ae484ffa21e542ccdca7adf5801870b
stonxzz <cuentamlgpro@gmail.com>
2024-08-15 13:33:51 -0600

    feature: :sparkles: An example of how use js functions was made

--- fundamentosVue/mi-primer-vue/src/App.vue
+++ fundamentosVue/mi-primer-vue/src/App.vue
@@ -1,11 +1,18 @@
 <script setup>
 const name = 'Vue dinamico'
 const styleColor = "color:blue"
+const arrayColores = ['blue', 'red', 'peru']
+const activo=true;
 </script>
 
 <template>
-  <h1>Hola {{name}}</h1>
-  <h2 :style="styleColor">Soy azul</h2>//: estos dos puntos es la abreviacion del vbind
+  <h1>Hola {{name.toUpperCase()}}</h1>
+  <h2>{{ arrayColores }}</h2>
+  <!-- //: estos dos puntos es la abreviacion del vbind -->
+  <h2 :style="`color: ${arrayColores[2]}`">`Soy peru</h2>
+  <h2>
+    {{ activo ? 'Estoy activo ' : 'Estoy inactivo' }}
+  </h2>
 </template>
 
 <style>
