369320eae4ab80ba89a8f83848c3666038c0949b
stonxzz <cuentamlgpro@gmail.com>
2024-08-15 15:17:56 -0600

    feature: :sparkles: An practice exercise of vfor was made

--- fundamentosVue/mi-primer-vue/src/App.vue
+++ fundamentosVue/mi-primer-vue/src/App.vue
@@ -3,14 +3,30 @@ const name = 'Vue dinamico'
 // const styleColor = "color:blue"
 // const arrayColores = ['blue', 'red', 'peru']
 // const activo=true;
-const arrayFrutas = ["🍎", "🍌", "🍉", "🍓", "🍒"];
+const arrayFrutas = [
+  {
+    name: "Manzana",
+    price: "$1.00",
+    description: "Una manzana",
+  },
+  {
+    name: "Pera",
+    price: "$2.00",
+    description: "Una pera",
+  },
+  {
+    name: "Naranja",
+    price: "$3.00",
+    description: "Una naranja",
+  },
+];
 </script>
 
 <template>
   <h1>Hola {{name.toUpperCase()}}</h1>
   <ul>
-    <li v-for="(fruta, index) in arrayFrutas":key="index">
-      {{index}}  {{ fruta }}
+    <li v-for="(fruta) in arrayFrutas":key="fruta.name">
+      {{ fruta.name }} - {{ fruta.price }} - {{ fruta.description }} 
     </li>
   </ul>
   <!-- <h2>{{ arrayColores }}</h2> -->
