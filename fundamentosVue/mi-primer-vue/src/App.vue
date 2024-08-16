7f9c900330dfa9c84e9676368dea6322f55961c3
stonxzz <cuentamlgpro@gmail.com>
2024-08-16 14:48:57 -0600

    feature: :sparkles: A practical activity was done where reactivity was used to change the value of a variable and to change the class of an element

--- fundamentosVue/mi-primer-vue/src/App.vue
+++ fundamentosVue/mi-primer-vue/src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import{ref} from 'vue'
+import{cloneVNode, ref} from 'vue'
 
 const nombre = 'vue dinamico'
 
@@ -7,17 +7,47 @@ let counter= ref(0)
 const increment = ()=>{
   counter.value++
 }
+
+const decrement =()=>{
+  counter.value--
+}
+
+const reset = ()=>{
+  counter.value = 0
+}
+
+// const changeCounterColor =()=>{
+//   let styleColor = ref('')
+//   if(counter.value>0){
+//     styleColor.value = "color: green"
+//   }else{
+//     styleColor.value = "color: red"
+//   }
+//   return styleColor.value
+// }
+
+
 </script>
 
 <template>
 <h1>Hola {{ nombre.toUpperCase() }}</h1>
-<h2>{{ counter }}</h2>
+<h2 :class="counter >0 ? 'positive' : 'negative'">{{ counter }}</h2>
 <button @click="increment">Aumentar</button>
+<button @click="decrement">Decrementar</button>
+<button @click="reset">Reset</button>
 
 </template>
 
 <style>
   h1{
+    color: red;
+  }
+
+  .positive{
+    color: green;
+  }
+
+  .negative{
     color: red;
   }
 </style>
