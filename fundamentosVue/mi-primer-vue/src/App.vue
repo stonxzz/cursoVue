84e220d8117c013f24793bb13aa1228e971a663b
stonxzz <cuentamlgpro@gmail.com>
2024-08-16 13:08:28 -0600

    feature: :sparkles: An example was made of how to use the event click(v-on)

--- fundamentosVue/mi-primer-vue/src/App.vue
+++ fundamentosVue/mi-primer-vue/src/App.vue
@@ -1,65 +1,18 @@
 <script setup>
-const activo = true
-const name = 'Vue dinamico'
-// const styleColor = "color:blue"
-// const arrayColores = ['blue', 'red', 'peru']
-// const activo=true;
-// const objetoFruta = 
-//   {
-//     name: "Manzana",
-//     price: "$1.00",
-//     description: "Una manzana",
-//     id: 1
-//   }
 
-const arrayFrutas = [
-  {
-    name: "Manzana",
-    price: "$1.00",
-    description: "Una manzana",
-    stock: 0,
-  },
-  {
-    name: "Pera",
-    price: "$2.00",
-    description: "Una pera",
-    stock: 10,
-  },
-  {
-    name: "Naranja",
-    price: "$3.00",
-    description: "Una naranja",
-    stock: 20,
-  },
-];
+const name = 'Vue dinamico'
+
+//Metodo - methods
+const handleClick = (message)=>{
+  console.log(message)
+}
 
 </script>
 
 <template>
   <h1>Hola {{name.toUpperCase()}}</h1>
-  
-  <ul>
-    <!-- recorrer objetos -->
-    <!-- <li v-for="(value, propiedad, index) in objetoFruta" :key="value">
-        {{ index }} - {{ propiedad }} : {{value}} 
-     </li> -->
-     <template v-for="(item) in arrayFrutas" :key="item.name">
-    <li v-if="item.stock>0">
-      {{ item.name }} - {{ item.price }}
-    </li>
-     </template>
-  </ul>
-  <!-- <h2>{{ arrayColores }}</h2> -->
-  <!-- //: estos dos puntos es la abreviacion del vbind
-  <h2 :style="`color: ${arrayColores[2]}`">`Soy peru</h2>
-  <h2 v-if="activo ===  true">
-    <span>Icono</span>
-    Estoy activo
-  </h2>
-  <p v-else-if="activo === false">Estoy inactivo</p>
-  <p v-else>Estoy indeciso</p>
-
-  <h2 v-show="activo">Estoy activo v-show</h2> -->
+  <button v-on:click="handleClick('Texto1')">Activame 1</button>
+  <button @click="handleClick('texto2')">Activame 2</button>
 
 </template>
 
