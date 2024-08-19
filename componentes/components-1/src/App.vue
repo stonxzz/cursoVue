c02e27da074045a78ed1013e831c8f8fcc341d97
stonxzz <cuentamlgpro@gmail.com>
2024-08-19 15:57:13 -0600

    feature: :sparkles: An example was made of how to use for in a component

--- componentes/components-1/src/App.vue
+++ componentes/components-1/src/App.vue
@@ -1,6 +1,14 @@
 <script setup>
 import ButtonCounter from "./components/ButtonCounter.vue";
 import BlogPost from "./components/BlogPost.vue";
+import { ref, registerRuntimeCompiler } from "vue";
+
+const posts = ref([
+  {title: 'post 1', id: 1, body: 'descripcion 1'},
+  {title: 'post 2', id: 2, body: 'descripcion 2'},
+  {title: 'post 3', id: 3, body: 'descripcion 3'},
+  {title: 'post 4', id: 4},
+])
 </script>
 
 <template>
@@ -8,8 +16,10 @@ import BlogPost from "./components/BlogPost.vue";
   <ButtonCounter />
   <ButtonCounter />
 
-  <BlogPost title="Post 1" :id="1" body="descripcion 1"></BlogPost>
-  <BlogPost title="Post 2" :id="2" body="descripcion 2"></BlogPost>
-  <BlogPost title="Post 3" :id="3" body="descripcion 3"></BlogPost>
-  <BlogPost title="Post 4" :id="4"></BlogPost>
+  <BlogPost 
+  v-for="(post) in posts" :key="post.id"
+  :title="post.title" 
+  :id="post.id" 
+  :body="post.body"></BlogPost>
+  
 </template>
