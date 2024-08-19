2a6d5e4609c8b633cb8b8422afe5277455477683
stonxzz <cuentamlgpro@gmail.com>
2024-08-19 15:09:22 -0600

    feature: :sparkles: An example was made of how to define and create props

--- componentes/components-1/src/App.vue
+++ componentes/components-1/src/App.vue
@@ -1,9 +1,18 @@
 <script setup>
-import ButtonCounter from './components/ButtonCounter.vue';
+import ButtonCounter from "./components/ButtonCounter.vue";
+import BlogPost from "./components/BlogPost.vue";
 </script>
 
 <template>
   <h1>APP</h1>
-  <ButtonCounter/>
-  <ButtonCounter/>
+  <ButtonCounter />
+  <ButtonCounter />
+
+  <BlogPost title="Post 1"
+  content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero iste voluptas ut ad 
+  pariatur at vitae voluptates, 
+  labore consectetur vel similique totam illum vero alias. Magnam expedita amet ipsa. Obcaecati."></BlogPost>
+  <BlogPost title="Post 2"></BlogPost>
+  <BlogPost title="Post 3"></BlogPost>
+  <BlogPost title="Post 4"></BlogPost>
 </template>
---
+++ componentes/components-1/src/components/BlogPost.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+defineProps(['title', 'content'])
+</script>
+
+<template>
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">{{title}}</h5>
+            <p>{{content}}</p>
+        </div>
+    </div>
+</template>
