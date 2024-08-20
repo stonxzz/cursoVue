ea028ee424e1c0a656ac97a94fd53d6770e8b45a
stonxzz <cuentamlgpro@gmail.com>
2024-08-20 12:48:55 -0600

    feature: :sparkles: An example was made of how to emit events between components

--- componentes/components-1/src/App.vue
+++ componentes/components-1/src/App.vue
@@ -4,22 +4,30 @@ import BlogPost from "./components/BlogPost.vue";
 import { ref, registerRuntimeCompiler } from "vue";
 
 const posts = ref([
-  {title: 'post 1', id: 1, body: 'descripcion 1'},
-  {title: 'post 2', id: 2, body: 'descripcion 2'},
-  {title: 'post 3', id: 3, body: 'descripcion 3'},
-  {title: 'post 4', id: 4},
-])
+  { title: "post 1", id: 1, body: "descripcion 1" },
+  { title: "post 2", id: 2, body: "descripcion 2" },
+  { title: "post 3", id: 3, body: "descripcion 3" },
+  { title: "post 4", id: 4 },
+]);
+
+const favorito = ref('')
+const cambiarFavorito = (title)=>{
+  favorito.value = title
+}
 </script>
 
 <template>
-  <h1>APP</h1>
-  <ButtonCounter />
-  <ButtonCounter />
-
-  <BlogPost 
-  v-for="(post) in posts" :key="post.id"
-  :title="post.title" 
-  :id="post.id" 
-  :body="post.body"></BlogPost>
-  
+  <div class="container">
+    <h1>APP</h1>
+    <h2>Mi post favorito: {{ favorito }}</h2>
+    
+    <BlogPost
+      v-for="post in posts"
+      :key="post.id"
+      :title="post.title"
+      :id="post.id"
+      :body="post.body"
+      @cambiarFavorito="cambiarFavorito"
+    ></BlogPost>
+  </div>
 </template>
--- componentes/components-1/src/components/BlogPost.vue
+++ componentes/components-1/src/components/BlogPost.vue
@@ -17,6 +17,7 @@ defineProps({
     <div class="card-body">
       <h5 class="card-title">{{ id }} - {{ title }}</h5>
       <p>{{ body }}</p>
+      <button @click="$emit('cambiarFavorito', title)" class="btn btn-outline-primary">Favorito</button>
     </div>
   </div>
 </template>
