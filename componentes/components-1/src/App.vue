17d3e6eaa63f9b563b70587acbfa4df426cb0d9f
stonxzz <cuentamlgpro@gmail.com>
2024-08-20 15:41:33 -0600

    feature: :sparkles: Added a loading screen, to be displayed when the promise is loaded

--- componentes/components-1/src/App.vue
+++ componentes/components-1/src/App.vue
@@ -4,12 +4,14 @@ import { computed, ref } from "vue";
 import ButtonCounter from "./components/ButtonCounter.vue";
 import PaginatePost from "./components/PaginatePost.vue";
 import BlogPost from "./components/BlogPost.vue";
+import LoadingSpinner from "./components/LoadingSpinner.vue";
 
 
 const posts = ref([]);
 const postXpage = 10
 const inicio = ref(0)
 const fin = ref(postXpage)
+const loading = ref(true)
 
 const favorito = ref("");
 const cambiarFavorito = (title) => {
@@ -32,10 +34,18 @@ fetch("https://jsonplaceholder.typicode.com/posts")
 .then((res) => res.json())
 .then((data) => {
   posts.value = data})
+.catch((e)=>console.log(e))
+.finally(()=>{
+  setTimeout(()=>{
+    loading.value=false
+  },2000)
+})
 </script>
 
 <template>
-  <div class="container">
+  <LoadingSpinner v-if="loading"/>
+
+  <div class="container" v-else>
     <h1>APP</h1>
     <h2>
       Mi post favorito:
