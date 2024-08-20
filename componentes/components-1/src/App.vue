9f238f7af0f2dd7d61302a26b8542ce38a041436
stonxzz <cuentamlgpro@gmail.com>
2024-08-20 14:18:10 -0600

    feature: :sparkles: An example was made of how to consume an API with fetch

--- componentes/components-1/src/App.vue
+++ componentes/components-1/src/App.vue
@@ -3,27 +3,29 @@ import ButtonCounter from "./components/ButtonCounter.vue";
 import BlogPost from "./components/BlogPost.vue";
 import { ref, registerRuntimeCompiler } from "vue";
 
-const posts = ref([
-  { title: "post 1", id: 1, body: "descripcion 1" },
-  { title: "post 2", id: 2, body: "descripcion 2" },
-  { title: "post 3", id: 3, body: "descripcion 3" },
-  { title: "post 4", id: 4 },
-]);
+const posts = ref([]);
 
-const favorito = ref('')
-const cambiarFavorito = (title)=>{
-  favorito.value = title
-}
+const favorito = ref("");
+const cambiarFavorito = (title) => {
+  favorito.value = title;
+};
+
+fetch("https://jsonplaceholder.typicode.com/posts")
+.then((res) => res.json())
+.then((data) => {
+  posts.value = data})
 </script>
 
 <template>
   <div class="container">
     <h1>APP</h1>
-    <h2>Mi post favorito:
+    <h2>
+      Mi post favorito:
       <span v-if="!favorito"><h2>No hay un post favorito</h2></span>
-      <span  v-else></span><h2>{{ favorito }}</h2>
+      <span v-else></span>
+      <h2>{{ favorito }}</h2>
     </h2>
-    
+
     <BlogPost
       v-for="post in posts"
       :key="post.id"
