ba892a4700f2cfebb3139cd22a37afba2cf99027
stonxzz <cuentamlgpro@gmail.com>
2024-08-21 13:23:45 -0600

    feature: :sparkles: Pokemon API was used using Axios, how to install Axios was shown

--- router-1/src/views/PokemonsView.vue
+++ router-1/src/views/PokemonsView.vue
@@ -1,3 +1,21 @@
+<script setup>
+    import axios from 'axios';
+    import { ref } from 'vue';
+
+    const pokemons = ref([])
+
+    const getData = async()=>{
+        try {
+            const {data} = await axios.get('https://pokeapi.co/api/v2/pokemon')
+            console.log(data.results)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    getData()
+</script>
+
 <template>
     <h1>Pokemons</h1>
 </template>
