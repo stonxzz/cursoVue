f8b732e2c8c9a6f130becd7cd16f32d4c72e0de6
stonxzz <cuentamlgpro@gmail.com>
2024-08-21 13:12:17 -0600

    feature: :sparkles: The first route for the Pokemon view was created

--- router-1/src/App.vue
+++ router-1/src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
@@ -14,7 +14,6 @@ import HelloWorld from './components/HelloWorld.vue'
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
         <RouterLink to="/pokemons">pokemons</RouterLink>
-
       </nav>
     </div>
   </header>
