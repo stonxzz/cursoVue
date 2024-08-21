3825cb40942b5dde4715ff5fce8dcef21c088dca
stonxzz <cuentamlgpro@gmail.com>
2024-08-21 15:01:54 -0600

    feature: :sparkles: The view was configured to make a request to the API of each individual pokemon using axios

--- router-1/src/views/PokeView.vue
+++ router-1/src/views/PokeView.vue
@@ -1,3 +1,19 @@
+<script setup>
+
+    import axios from 'axios';
+
+    const getData = async()=>{
+        try {
+            const {data} = await axios.get(`https://pokeapi.co/api/v2/pokemon/pikachu`)
+            console.log(data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    getData()
+</script>
+
 <template>
     <h1>Poke name: {{ $route.params.pokename }}</h1>
 </template>
